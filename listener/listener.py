6672b03f0d93b6f6c3957e95886ca8bd88a37b37
Benedito Marques <benedito@fortics.com.br>
2026-03-25 19:47:31 -0300

    fix: fix listener

--- listener/listener.py
+++ listener/listener.py
@@ -7,8 +7,8 @@ r = redis.Redis(host='redis', port=6379, decode_responses=True)
 
 async def ami_send(asterisk_host, tenant, extension, status):
     try:
-        ami = AMIClient(address=asterisk_host, username='admin', secret='admin')
-        await ami.login()
+        ami = AMIClient(address=asterisk_host,port=5038)
+        ami.login(username='admin', secret='admin')
         
         state = "NOT_INUSE" if status == "registered" else "UNAVAILABLE"
         custom = f"REG-{tenant}-{extension}"
@@ -16,9 +16,14 @@ async def ami_send(asterisk_host, tenant, extension, status):
         action = SimpleAction('Setvar',
                               Variable=f"DEVICE_STATE(Custom:{custom})",
                               Value=state)
-        response = await ami.send_action(action)
+        #response = await ami.send_action(action)
+        future = ami.send_action(action)
+        response = future.response
+
         print(f"✓ {asterisk_host} → Custom:{custom} = {state} | {response}")
-        await ami.logoff()
+        #await ami.logoff()
+        future_logoff = ami.logoff()
+        response_logoff = future_logoff.response
     except Exception as e:
         print(f"✗ Erro AMI {asterisk_host}: {e}")
 
